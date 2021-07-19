553b42494abf7c66ac7347757041d994b93ae60b
machntek <machntek@gmail.com>
2021-07-19 23:51:03 +0900

    first commit

---
+++ src/main/java/com/machntek/community/CommunityApplicationJavaApplication.java
@@ -0,0 +1,13 @@
+package com.machntek.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplicationJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplicationJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/machntek/community/CommunityApplicationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machntek.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
